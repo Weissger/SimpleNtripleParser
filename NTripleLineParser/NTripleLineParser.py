69e0feccb0e2e9091c0e7d41a051bb1f86f92899
tmy <weissger@fim.uni-passau.de>
2016-01-12 13:10:35 +0100

    Initial commit

---
+++ NTripleLineParser/NTripleLineParser.py
@@ -0,0 +1,53 @@
+__author__ = 'tmy'
+
+COMMENT_INDICATOR = '#'
+URI_INDICATOR = '<'
+
+
+class NTripleLineParser():
+    def __init__(self, separator):
+        self.separator = separator
+
+    @staticmethod
+    def __is_comment_line(line):
+        return line.startswith(COMMENT_INDICATOR)
+
+    @staticmethod
+    def __strip_uri_indicator(uri):
+        if uri.startswith(URI_INDICATOR):
+            return uri[1:-1]
+        else:
+            return uri
+
+    def is_comment_line(self, line):
+        line = line.strip()
+        return self.__is_comment_line(line)
+
+    def get_subject(self, line):
+        line = line.strip()
+        if self.__is_comment_line(line):
+            return None
+        else:
+            return line.split(self.separator, 1)[0][1:-1]
+
+    def get_predicate(self, line):
+        line = line.strip()
+        if self.__is_comment_line(line):
+            return None
+        else:
+            return line.split(self.separator, 2)[1][1:-1]
+
+    def get_object(self, line):
+        line = line.strip()
+        if self.__is_comment_line(line):
+            return None
+        else:
+            return line.split(self.separator, 3)[2][1:-2]
+
+    def get_triple(self, line):
+        split_line = line.split(self.separator)
+        if split_line[0].strip().startswith("#"):
+            return None
+        else:
+            return {"subject": split_line[0][1:-1], "predicate": split_line[1][1:-1],
+                    "object": split_line[2][1:-2]}
---
+++ tests/test_nTripleParser.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from NTripleLineParser.NtripleLineParser import NtripleParser
+
+__author__ = 'tmy'
+
+lines = ["# This is a comment",
+         "<http://www.test.org/subject> <http://www.test.org/predicate> <http://www.test.org/object>."]
+
+
+class TestNtripleParser(TestCase):
+    def setUp(self):
+        self.parser = NtripleParser(" ")
+
+    def test_is_comment_line(self):
+        self.assertTrue(self.parser.is_comment_line(lines[0]))
+        self.assertFalse(self.parser.is_comment_line(lines[1]))
+
+    def test_get_subject_comment(self):
+        self.assertEqual(None, self.parser.get_subject(lines[0]))
+
+    def test_get_subject(self):
+        self.assertEqual("http://www.test.org/subject", self.parser.get_subject(lines[1]))
+
+    def test_get_predicate_comment(self):
+        self.assertEqual(None, self.parser.get_predicate(lines[0]))
+
+    def test_get_predicate(self):
+        self.assertEqual("http://www.test.org/predicate", self.parser.get_predicate(lines[1]))
+
+    def test_get_object_comment(self):
+        self.assertEqual(None, self.parser.get_object(lines[0]))
+
+    def test_get_object(self):
+        self.assertEqual("http://www.test.org/object", self.parser.get_object(lines[1]))
+
+    def test_get_triple(self):
+        triple = self.parser.get_triple(lines[1])
+        self.assertEqual("http://www.test.org/subject", triple["subject"])
+        self.assertEqual("http://www.test.org/predicate", triple["predicate"])
+        self.assertEqual("http://www.test.org/object", triple["object"])
